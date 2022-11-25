3cd2c4b9306e5c1ce99a01d5021ad51dc1b1c345
seheon Kim <seheon.emma@gmail.com>
2022-11-25 22:58:16 +1000

    feat. selection sort

--- src/Sort/SelectionSort.java
+++ src/Sort/SelectionSort.java
@@ -3,14 +3,20 @@ package Sort;
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int min = 0;
-        int temp = 0;
-        int index = 0;
-        int[] array = {3,4,2,1,5,7,9,8,10,6};        
-    
+
+
+        // ex) 10 + 9 + 8 + 7 + ... + 1
+        // N * (N+1)/2
+        // O(N*N)
+        // O(N^2)
+
+        int []array = {10,2,5,4,7,8,48,55,76,44,9,1,6,3,11};
+        int index=0;
+        int temp;
+        int min;
         for(int i = 0; i < array.length; i++){
-            min = 99;
-            for(int j = i; j < array.length; j++){
+            min = 9999;
+            for(int j = i; j<array.length; j++){
                 if(min > array[j]){
                     min = array[j];
                     index = j;
@@ -20,7 +26,6 @@ public class SelectionSort {
             array[i] = min;
             array[index] = temp;
         }
-
         for(int i = 0; i < array.length; i++){
            System.out.println(array[i]);
         }
