4a0f6b0c9365c65db7872943e8cabfd4dc24c10d
seheon Kim <seheon.emma@gmail.com>
2023-01-10 15:51:45 +1000

    feat. Heap

---
+++ src/DFS_BFS/DFS.java
@@ -0,0 +1,16 @@
+package DFS_BFS;
+
+import java.util.Stack;
+
+public class DFS {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<Integer>();
+
+        int N = 18;
+        while(N>0){
+            N = N-3;
+            System.out.println("N : "+N);
+        }
+
+    }
+}
---
+++ src/DFS_BFS/Main.java
@@ -0,0 +1,18 @@
+package DFS_BFS;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[]arr = new int[28];
+        for(int i = 0; i < 5; i++){
+            arr[i] = s.nextInt();
+        }
+        for(int i : arr){
+            System.out.println(i);
+        }
+
+
+    }
+}
